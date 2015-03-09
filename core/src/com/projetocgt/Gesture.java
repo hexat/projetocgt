0608adfd16dc1b9c0759413a79e36d3656014c67
Vitor Aquino <vitordeaquino@gmail.com>
2015-03-09 16:25:33 -0300

    gesture

--- core/src/com/projetocgt/Gesture.java
+++ core/src/com/projetocgt/Gesture.java
@@ -38,7 +38,6 @@ public class Gesture implements GestureListener{
 			return false;
 		} else{
 			game.getController().activateKey(InputPolicy.TAP);
-			System.out.println("TAP");
 			return true;
 		}
 		
@@ -48,7 +47,6 @@ public class Gesture implements GestureListener{
 	public boolean longPress(float x, float y) {
 		longTap = true;
 		game.getController().activateKey(InputPolicy.HOLD_TAP);
-		System.out.println("HOLD TAP");
 		return true;
 	}
 	
@@ -81,55 +79,29 @@ public class Gesture implements GestureListener{
 		}
 		
 		return true;
-//		if(Math.abs(deltaX)> 5 || Math.abs(deltaY) > 5){
-//			if (Math.abs(deltaX) >= Math.abs(deltaY)){
-//				game.getController().releaseSlices();
-//				if(deltaX > 0){
-//					game.getController().activateKey(InputPolicy.SLIDE_RIGHT);
-////					System.out.println("SLIDE RIGHT");
-//				} else {
-//					game.getController().activateKey(InputPolicy.SLIDE_LEFT);
-////					System.out.println("SLIDE LEFT");
-//				}
-//			} else {
-//				if(deltaY > 0){
-//					game.getController().activateKey(InputPolicy.SLIDE_DOWN);
-////					System.out.println("SLIDE DOWN");
-//					
-//				} else {
-//					game.getController().activateKey(InputPolicy.SLIDE_UP);
-////					System.out.println("SLIDE UP");
-//				}
-//			}
-//			
-//			
-//		} 
-//		return true;
+
 	}
 	
 	@Override
 	public boolean fling(float velocityX, float velocityY, int button) {
-		System.out.println("FLIG");
 		return false;
 	}
 
 	@Override
 	public boolean panStop(float x, float y, int pointer, int button) {
 		game.getController().releaseSlices();
-		System.out.println("OIIII");
 		return false;
 	}
 
 	@Override
 	public boolean zoom(float initialDistance, float distance) {
-		System.out.println("ZOOM");
+		
 		return false;
 	}
 
 	@Override
 	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
 			Vector2 pointer1, Vector2 pointer2) {
-		System.out.println("PINCH");
 		return false;
 	}
 
