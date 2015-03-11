aebdeb4ae1d0ad7c3448086f5f227fa72179b26f
Luan James <capjam@gmail.com>
2015-03-11 13:26:44 -0300

    grouppane layout

--- ferramenta/src/br/edu/ifce/cgt/application/controller/titleds/GroupLifeBarTitledPane.java
+++ ferramenta/src/br/edu/ifce/cgt/application/controller/titleds/GroupLifeBarTitledPane.java
@@ -19,8 +19,8 @@ import javafx.scene.layout.VBox;
 public class GroupLifeBarTitledPane extends TitledPane {
     private final LifebarPane lifeBarPane;
     private final EnemyGroupLifeBar enemyGroupLifeBar;
-    private final ComboBox<String> boxLabelObjects;
-    private final VBox panEnemies;
+//    private final ComboBox<String> boxLabelObjects;
+//    private final VBox panEnemies;
 
     public GroupLifeBarTitledPane(final EnemyGroupLifeBar enemyGroupLifeBar) {
         this.enemyGroupLifeBar = enemyGroupLifeBar;
@@ -28,61 +28,61 @@ public class GroupLifeBarTitledPane extends TitledPane {
         setText("Configurações LifeBar de Inimigos");
         VBox layout = new VBox();
 
-        GridPane gridPane = new GridPane();
+//        GridPane gridPane = new GridPane();
+//
+//        ColumnConstraints col1 = new ColumnConstraints();
+//        col1.setPercentWidth(40);
+//        ColumnConstraints col2 = new ColumnConstraints();
+//        col2.setPercentWidth(30);
+//        ColumnConstraints col3 = new ColumnConstraints();
+//        col3.setPercentWidth(30);
+//        gridPane.getColumnConstraints().addAll(col1,col2,col3);
+//
+//        Label label = new Label("Objeto");
+//        gridPane.add(label, 0, 0);
 
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setPercentWidth(40);
-        ColumnConstraints col2 = new ColumnConstraints();
-        col2.setPercentWidth(30);
-        ColumnConstraints col3 = new ColumnConstraints();
-        col3.setPercentWidth(30);
-        gridPane.getColumnConstraints().addAll(col1,col2,col3);
+//        boxLabelObjects = new ComboBox<String>();
+//        boxLabelObjects.getItems().addAll(enemyGroupLifeBar.getWorld().getEnemyLabels());
+//
+//        panEnemies = new VBox();
+//
+//        boxLabelObjects.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent event) {
+//                for (String label : enemyGroupLifeBar.getWorld().getEnemyLabels()) {
+//                    if (!boxLabelObjects.getItems().contains(label) && !enemyGroupLifeBar.contains(label)) {
+//                        boxLabelObjects.getItems().add(label);
+//                    }
+//                }
+//            }
+//        });
 
-        Label label = new Label("Objeto");
-        gridPane.add(label, 0, 0);
+//        boxLabelObjects.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                final String id = boxLabelObjects.getSelectionModel().getSelectedItem();
+//                if (enemyGroupLifeBar.contains(id)) {
+//                    enemyGroupLifeBar.addEnemy(id);
+//                    final ItemViewPane pane = new ItemViewPane(id);
+//                    pane.getDeleteButton().setOnAction(new EventHandler<ActionEvent>() {
+//                        @Override
+//                        public void handle(ActionEvent event) {
+//                            enemyGroupLifeBar.removeEnemy(id);
+//                            panEnemies.getChildren().remove(pane);
+//                        }
+//                    });
+//                    panEnemies.getChildren().add(pane);
+//                }
+//            }
+//        });
 
-        boxLabelObjects = new ComboBox<String>();
-        boxLabelObjects.getItems().addAll(enemyGroupLifeBar.getWorld().getEnemyLabels());
+//        gridPane.add(boxLabelObjects, 1, 0);
+//        layout.getChildren().add(gridPane);
+//        layout.getChildren().add(panEnemies);
 
-        panEnemies = new VBox();
-
-        boxLabelObjects.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                for (String label : enemyGroupLifeBar.getWorld().getEnemyLabels()) {
-                    if (!boxLabelObjects.getItems().contains(label) && !enemyGroupLifeBar.contains(label)) {
-                        boxLabelObjects.getItems().add(label);
-                    }
-                }
-            }
-        });
-
-        boxLabelObjects.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                final String id = boxLabelObjects.getSelectionModel().getSelectedItem();
-                if (enemyGroupLifeBar.contains(id)) {
-                    enemyGroupLifeBar.addEnemy(id);
-                    final ItemViewPane pane = new ItemViewPane(id);
-                    pane.getDeleteButton().setOnAction(new EventHandler<ActionEvent>() {
-                        @Override
-                        public void handle(ActionEvent event) {
-                            enemyGroupLifeBar.removeEnemy(id);
-                            panEnemies.getChildren().remove(pane);
-                        }
-                    });
-                    panEnemies.getChildren().add(pane);
-                }
-            }
-        });
-
-        gridPane.add(boxLabelObjects, 1, 0);
-        layout.getChildren().add(gridPane);
-        layout.getChildren().add(panEnemies);
-
-        Separator separator = new Separator(Orientation.HORIZONTAL);
-        separator.setPadding(new Insets(5, 0, 5, 0));
-        layout.getChildren().add(separator);
+//        Separator separator = new Separator(Orientation.HORIZONTAL);
+//        separator.setPadding(new Insets(5, 0, 5, 0));
+//        layout.getChildren().add(separator);
         layout.setPadding(new Insets(5));
 
         lifeBarPane = new LifebarPane(enemyGroupLifeBar);
