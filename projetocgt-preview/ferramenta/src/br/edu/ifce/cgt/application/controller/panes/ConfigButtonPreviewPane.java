2335c37d146e70eb46714b1af8b134b4a285ed58
EdyJunior <dujunior1300@gmail.com>
2015-09-28 10:46:07 -0300

    Parte do preview screen implementado

--- projetocgt-preview/ferramenta/src/br/edu/ifce/cgt/application/controller/panes/ConfigButtonPreviewPane.java
+++ projetocgt-preview/ferramenta/src/br/edu/ifce/cgt/application/controller/panes/ConfigButtonPreviewPane.java
@@ -2,7 +2,10 @@ package br.edu.ifce.cgt.application.controller.panes;
 
 import br.edu.ifce.cgt.application.Main;
 import br.edu.ifce.cgt.application.controller.ui.FloatTextField;
+import br.edu.ifce.cgt.application.util.Config;
+import br.edu.ifce.cgt.application.util.DialogsUtil;
 import br.edu.ifce.cgt.application.vo.CGTButtonScreenPreview;
+import cgt.util.CGTTexture;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -20,7 +24,7 @@ import java.io.IOException;
 public class ConfigButtonPreviewPane extends Accordion {
 
     @FXML
-    public TextField textura;
+    public TextField texture;
     @FXML
     private TextField texPres;
     @FXML
@@ -31,10 +35,10 @@ public class ConfigButtonPreviewPane extends Accordion {
     private FloatTextField RelX;
     @FXML
     private FloatTextField RelY;
-    @FXML
+    /*@FXML
     private Button p2;
     @FXML
-    private Button p1;
+    private Button p1;*/
     @FXML
     private ComboBox<String> choices;
     private CGTButtonScreenPreview btn;
@@ -59,8 +63,52 @@ public class ConfigButtonPreviewPane extends Accordion {
         this.onUpdateRunnable = onUpdateRunnable;
     }
 
-    @FXML public void Search(ActionEvent actionEvent) {
-        btn.getButton().setTextureUp();
+    @FXML public void Search1(ActionEvent actionEvent) {
+        File chosenFile = DialogsUtil.showOpenDialog("Selecionar imagem do botao", DialogsUtil.IMG_FILTER);
+
+        String path = "";
+
+        if (chosenFile != null) {
+            btn.getButton().setTextureUp(new CGTTexture(Config.get().createImg(chosenFile)));
+            path = chosenFile.getName();
+        }
+
+        texture.setText(path);
+
+        if (onUpdateRunnable != null)
+            onUpdateRunnable.run();
     }
+    @FXML public void Search2(ActionEvent actionEvent) {
+        File chosenFile = DialogsUtil.showOpenDialog("Selecionar imagem do botao pressionado", DialogsUtil.IMG_FILTER);
+
+        String path = "";
+
+        if (chosenFile != null) {
+            btn.getButton().setTextureDown(new CGTTexture(Config.get().createImg(chosenFile)));
+            path = chosenFile.getName();
+        }
+
+        texPres.setText(path);
+
+        if (onUpdateRunnable != null)
+            onUpdateRunnable.run();
+    }
+
+    @FXML public void BX(ActionEvent actionEvent) {
+        if(RelX.getValue() != 0)
+            btn.getButton().setRelativeX(RelX.getValue());
+    }
+    @FXML public void BY(ActionEvent actionEvent) {
+        if(RelY.getValue() != 0)
+            btn.getButton().setRelativeY(RelY.getValue());
+    }
+    @FXML public void BW(ActionEvent actionEvent) {
+        if(WRel.getValue() != 0)
+            btn.getButton().setRelativeWidth(WRel.getValue());
+    }@FXML public void BH(ActionEvent actionEvent) {
+        if(HRel.getValue() != 0)
+            btn.getButton().setRelativeHeight(HRel.getValue());
+    }
+
 
 }
