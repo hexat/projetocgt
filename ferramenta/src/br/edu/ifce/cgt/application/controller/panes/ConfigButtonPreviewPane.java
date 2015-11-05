1a85acc9daf93040617b206f87afffd4c5611902
Joel Rocha <jrocha@cpqi.com>
2015-11-05 14:54:04 -0200

    Alteração de melhoria para o painel de configuração do botão da tela

--- ferramenta/src/br/edu/ifce/cgt/application/controller/panes/ConfigButtonPreviewPane.java
+++ ferramenta/src/br/edu/ifce/cgt/application/controller/panes/ConfigButtonPreviewPane.java
@@ -12,11 +12,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Accordion;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,27 +24,27 @@ import java.io.IOException;
 /**
  * Created by Edy Junior on 24/09/2015.
  */
-public class ConfigButtonPreviewPane extends Accordion {
+public class ConfigButtonPreviewPane extends StackPane {
 
     @FXML
     private TextField textUp;
     @FXML
     private TextField textPres;
     @FXML
-    private FloatTextField HRel;
+    private FloatTextField hRel;
     @FXML
-    private FloatTextField WRel;
+    private FloatTextField wRel;
     @FXML
-    private FloatTextField RelX;
+    private FloatTextField relX;
     @FXML
-    private FloatTextField RelY;
+    private FloatTextField relY;
     @FXML
     private ComboBox<String> choices;
     private CGTButtonScreenPreview btn;
     private Runnable onUpdateRunnable;
     private double x = 0, y = 1.0f, w = 0.2f, h = 0.2f;
 
-    public ConfigButtonPreviewPane(CGTButtonScreenPreview btn){
+    public ConfigButtonPreviewPane(CGTButtonScreenPreview btn) {
         FXMLLoader xml2 = new FXMLLoader(Main.class.getResource("/view/ConfigButtonScreen2.fxml"));
         xml2.setController(this);
         xml2.setRoot(this);
@@ -60,20 +60,21 @@ public class ConfigButtonPreviewPane extends Accordion {
         init();
     }
 
-    public ConfigButtonPreviewPane(CGTButtonScreenPreview btn, Runnable onUpdateRunnable){
+    public ConfigButtonPreviewPane(CGTButtonScreenPreview btn, Runnable onUpdateRunnable) {
         this(btn);
         this.onUpdateRunnable = onUpdateRunnable;
     }
 
     private void init() {
-        choices.setOnMouseClicked(e-> {
+        choices.setOnMouseClicked(e -> {
             choices.getItems().clear();
             choices.getItems().addAll(Config.get().getGame().getIds());
         });
-        RelX.setValue(0.0f);
-        RelY.setValue(1.0f);
-        WRel.setValue(0.2f);
-        HRel.setValue(0.2f);
+
+        relX.setValue(0.0f);
+        relY.setValue(1.0f);
+        wRel.setValue(0.2f);
+        hRel.setValue(0.2f);
 
         choices.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -85,80 +86,77 @@ public class ConfigButtonPreviewPane extends Accordion {
         if (btn.getObject().getScreenToGo() != null) {
             choices.getSelectionModel().select(btn.getObject().getScreenToGo().getId());
         }
-        RelX.focusedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if (!newValue && RelX.getValue() >= 0 && RelX.getValue() <= 1 - WRel.getValue()) {
-                    btn.getImage().setX(RelX.getValue() * btn.getImage().getWidthBCKG());
-                    btn.getObject().setRelativeX(RelX.getValue());
-                    x = RelX.getValue();
-                }
 
-                else if(!newValue){
+        relX.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue && relX.getValue() >= 0 && relX.getValue() <= 1 - wRel.getValue()) {
+                    btn.getImage().setX(relX.getValue() * btn.getImage().getWidthBCKG());
+                    btn.getObject().setRelativeX(relX.getValue());
+                    x = relX.getValue();
+                } else if (!newValue) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setHeaderText("Coordenadas fora dos limites");
-                    alert.setContentText("Intervalo aceito: [0 ; " + (1 - WRel.getValue()) + "]");
+                    alert.setContentText("Intervalo aceito: [0 ; " + (1 - wRel.getValue()) + "]");
                     alert.show();
-                    btn.getObject().setRelativeX((float)x);
-                    RelX.setValue(x);
+                    btn.getObject().setRelativeX((float) x);
+                    relX.setValue(x);
                 }
                 if (onUpdateRunnable != null)
                     onUpdateRunnable.run();
             }
         });
-        RelY.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+        relY.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if (!newValue && RelY.getValue() >= HRel.getValue() && RelY.getValue() <= 1) {
-                    btn.getImage().setY((1 - RelY.getValue()) * btn.getImage().getHeightBCKG());
-                    btn.getObject().setRelativeY(1 - RelY.getValue());
-                    y = RelY.getValue();
-                }
-                else if(!newValue){
+                if (!newValue && relY.getValue() >= hRel.getValue() && relY.getValue() <= 1) {
+                    btn.getImage().setY((1 - relY.getValue()) * btn.getImage().getHeightBCKG());
+                    btn.getObject().setRelativeY(1 - relY.getValue());
+                    y = relY.getValue();
+                } else if (!newValue) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setHeaderText("Coordenadas fora dos limites");
-                    alert.setContentText("Intervalo aceito: [" + HRel.getValue() + " ; 1]");
+                    alert.setContentText("Intervalo aceito: [" + hRel.getValue() + " ; 1]");
                     alert.show();
-                    btn.getObject().setRelativeY(1.0f - (float)y);
-                    RelY.setValue(y);
+                    btn.getObject().setRelativeY(1.0f - (float) y);
+                    relY.setValue(y);
                 }
                 if (onUpdateRunnable != null)
                     onUpdateRunnable.run();
             }
         });
-        WRel.focusedProperty().addListener(new ChangeListener<Boolean>() {
+        wRel.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if (!newValue && WRel.getValue() >= 0 && WRel.getValue() <= 1 - RelX.getValue()) {
-                    btn.getObject().setRelativeWidth(WRel.getValue());
-                    w = WRel.getValue();
-                }
-                else if(!newValue){
+                if (!newValue && wRel.getValue() >= 0 && wRel.getValue() <= 1 - relX.getValue()) {
+                    btn.getObject().setRelativeWidth(wRel.getValue());
+                    w = wRel.getValue();
+                } else if (!newValue) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setHeaderText("Largura fora dos limites");
-                    alert.setContentText("Intervalo aceito: [0 ; " + (1 - RelX.getValue()) + "]");
+                    alert.setContentText("Intervalo aceito: [0 ; " + (1 - relX.getValue()) + "]");
                     alert.show();
-                    btn.getObject().setRelativeWidth((float)(w));
-                    WRel.setValue(w);
+                    btn.getObject().setRelativeWidth((float) (w));
+                    wRel.setValue(w);
                 }
                 if (onUpdateRunnable != null)
                     onUpdateRunnable.run();
             }
         });
-        HRel.focusedProperty().addListener(new ChangeListener<Boolean>() {
+        hRel.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if (!newValue && HRel.getValue() >= 0 && HRel.getValue() <= RelY.getValue()) {
-                    btn.getObject().setRelativeHeight(HRel.getValue());
-                    h = HRel.getValue();
-                }
-                else if(!newValue){
+                if (!newValue && hRel.getValue() >= 0 && hRel.getValue() <= relY.getValue()) {
+                    btn.getObject().setRelativeHeight(hRel.getValue());
+                    h = hRel.getValue();
+                } else if (!newValue) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setHeaderText("Largura fora dos limites");
-                    alert.setContentText("Intervalo aceito: [0 ; " + RelY.getValue() + "]");
+                    alert.setContentText("Intervalo aceito: [0 ; " + relY.getValue() + "]");
                     alert.show();
-                    btn.getObject().setRelativeHeight((float)(h));
-                    HRel.setValue(h);
+                    btn.getObject().setRelativeHeight((float) (h));
+                    hRel.setValue(h);
                 }
                 if (onUpdateRunnable != null)
                     onUpdateRunnable.run();
@@ -166,7 +164,8 @@ public class ConfigButtonPreviewPane extends Accordion {
         });
     }
 
-    @FXML public void Search1(ActionEvent actionEvent) {
+    @FXML
+    public void searchTextureUp(ActionEvent actionEvent) {
         File chosenFile = DialogsUtil.showOpenDialog("Selecionar imagem do botao", DialogsUtil.IMG_FILTER);
 
         String path = "";
@@ -183,7 +182,9 @@ public class ConfigButtonPreviewPane extends Accordion {
         if (onUpdateRunnable != null)
             onUpdateRunnable.run();
     }
-    @FXML public void Search2(ActionEvent actionEvent) {
+
+    @FXML
+    public void searchTexturePressed(ActionEvent actionEvent) {
         File chosenFile = DialogsUtil.showOpenDialog("Selecionar imagem do botao pressionado", DialogsUtil.IMG_FILTER);
 
         String path = "";
@@ -199,26 +200,31 @@ public class ConfigButtonPreviewPane extends Accordion {
             onUpdateRunnable.run();
     }
 
-    public TextField getTextUp(){
+    public TextField getTextUp() {
         return this.textUp;
     }
-    public TextField getTextPress(){
+
+    public TextField getTextPress() {
         return this.textPres;
     }
-    public FloatTextField getRelX(){
-        return this.RelX;
-    }
-    public FloatTextField getRelY(){
-        return this.RelY;
-    }
-    public FloatTextField getWRel(){
-        return this.WRel;
-    }
-    public FloatTextField getHRel(){
-        return this.HRel;
+
+    public FloatTextField getRelX() {
+        return this.relX;
     }
 
-    public ComboBox<String> getScreens(){
+    public FloatTextField getRelY() {
+        return this.relY;
+    }
+
+    public FloatTextField getWRel() {
+        return this.wRel;
+    }
+
+    public FloatTextField getHRel() {
+        return this.hRel;
+    }
+
+    public ComboBox<String> getScreens() {
         return this.choices;
     }
 
